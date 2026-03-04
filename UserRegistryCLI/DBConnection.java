04fdf05dd23667809740616d7c455d6d05f1c080
Abhiram <abhiramachana@gmail.com>
2026-03-04 17:01:24 +0530

    Java CLI + MySQL Connection

---
+++ UserRegistryCLI/DBConnection.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/user_registry";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Lfiabhi*12";
+
+    public static Connection getConnection() {
+
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
